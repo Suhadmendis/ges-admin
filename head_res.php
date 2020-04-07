f47bd9acce442d62823604a089aeb2dbcca56ea3
Suhadmendis <suhad.a.mendis@gmail.com>
2020-04-07 13:29:31 +0530

    dependace

--- head_res.php
+++ head_res.php
@@ -38,8 +38,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 
 
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="js/axios.min.js"></script>
+    <script src="js/vue.js"></script>
 
     
   <!-- transfer from the footer -->
